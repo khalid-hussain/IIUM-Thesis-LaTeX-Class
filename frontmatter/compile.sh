517876f8e92b005967f6a883bbc00c35589d0333
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-01-04 20:30:37 +0800

    Improved compile script for frontmatter

--- frontmatter/compile.sh
+++ frontmatter/compile.sh
@@ -1,4 +1,38 @@
-for i in *.tex; do pdflatex $i;done
+# Ensure you have "Traditional Arabic" font, otherwise compilation for Arabic
+# abstract will fail. You may also change the font from the TeX file instead.
 
-rm *.aux
-rm *.log
+clean(){
+  rm -f *.aux
+  rm -f *.log
+  rm -f *.bbl
+}
+
+compileArabic(){
+  # echo "Arabic"
+  xelatex arabicAbstractPage.tex
+  clean
+}
+
+compileEnglish(){
+  for i in *.tex; do
+    if [ "$i" != "arabicAbstractPage.tex" ]; then
+      # echo "$i"
+      pdflatex $i;
+    fi
+  done
+  clean
+}
+
+compileAll(){
+  compileNonArabic
+  compileArabic
+  clean
+}
+
+if [ "$1" == "arabic" ]; then
+  compileArabic
+elif [ "$1" == "english" ]; then
+  compileEnglish
+else
+  compileAll
+fi
